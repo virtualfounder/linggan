1567c787fd0dc36d115d0591a8da7ea98ca66bf8
virtualfounder <70188152+virtualfounder@users.noreply.github.com>
2024-04-20 16:27:51 +0000

    Deploying to gh-pages from @ virtualfounder/linggan@5b075f442fb2655b0c04fcb0d447590e733ec7ee 🚀

---
+++ assets/vidstack-Ccp8mxka-NcLN9y72.js
@@ -0,0 +1,1 @@
+import{bD as i,bE as t}from"./app-u8p_fo0q.js";class n{constructor(s){this.La=s}Xa(){i(this.ya)&&this.fg()}$(){t(this.ya)&&window.cancelAnimationFrame(this.ya),this.ya=void 0}fg(){this.ya=window.requestAnimationFrame(()=>{i(this.ya)||(this.La(),this.fg())})}}export{n as R};
