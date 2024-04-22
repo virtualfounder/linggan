3a5f9b802c0ad0d8fe3cd6c0b8efc07adcd3a6f6
virtualfound <2849492659@qq.com>
2024-04-22 12:40:48 +0800

    促使下fgh

--- blog/.vuepress/theme.ts
+++ blog/.vuepress/theme.ts
@@ -2,6 +2,7 @@ import { hopeTheme } from "vuepress-theme-hope";
 import { enNavbar, zhNavbar } from "./navbar/index.js";
 import { enSidebar, zhSidebar } from "./sidebar/index.js";
 import { MR_HOPE_AVATAR } from "./logo.js";
+import { docsearchPlugin } from '@vuepress/plugin-docsearch'
 
 export default hopeTheme({
   hostname: "https://mister-hope.github.io",
@@ -117,7 +118,7 @@ export default hopeTheme({
 
   plugins: {
     blog: true,
-
+    
     // Install @waline/client before enabling it
     // Note: This is for testing ONLY!
     // You MUST generate and use your own comment service in production.
@@ -125,7 +126,63 @@ export default hopeTheme({
       provider: "Waline",
       serverURL: "https://waline-comment.vuejs.press",
     },
+    docsearch:({
+      appId: "<APP_ID>",
+      apiKey: "<API_KEY>",
+      indexName: "<INDEX_NAME>",
 
+      locales: {
+        "/": {
+          placeholder: "搜索文档",
+          translations: {
+            button: {
+              buttonText: "搜索文档",
+              buttonAriaLabel: "搜索文档",
+            },
+            modal: {
+              searchBox: {
+                resetButtonTitle: "清除查询条件",
+                resetButtonAriaLabel: "清除查询条件",
+                cancelButtonText: "取消",
+                cancelButtonAriaLabel: "取消",
+              },
+              startScreen: {
+                recentSearchesTitle: "搜索历史",
+                noRecentSearchesText: "没有搜索历史",
+                saveRecentSearchButtonTitle: "保存至搜索历史",
+                removeRecentSearchButtonTitle: "从搜索历史中移除",
+                favoriteSearchesTitle: "收藏",
+                removeFavoriteSearchButtonTitle: "从收藏中移除",
+              },
+              errorScreen: {
+                titleText: "无法获取结果",
+                helpText: "你可能需要检查你的网络连接",
+              },
+              footer: {
+                selectText: "选择",
+                navigateText: "切换",
+                closeText: "关闭",
+                searchByText: "搜索提供者",
+              },
+              noResultsScreen: {
+                noResultsText: "无法找到相关结果",
+                suggestedQueryText: "你可以尝试查询",
+                reportMissingResultsText: "你认为该查询应该有结果？",
+                reportMissingResultsLinkText: "点击反馈",
+              },
+            },
+          },
+        },
+        "/en/": {
+          placeholder: "Search Documentation",
+          translations: {
+            button: {
+              buttonText: "Search Documentation",
+            },
+          },
+        },
+      },
+    }),
     components: {
       components: ["Badge", "VPCard",
       "ArtPlayer",
