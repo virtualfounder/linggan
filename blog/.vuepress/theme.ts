975fd3001916caf25b6d25b8997d4128b82e446c
virtualfound <2849492659@qq.com>
2024-05-18 23:01:02 +0800

    暗示

--- blog/.vuepress/theme.ts
+++ blog/.vuepress/theme.ts
@@ -214,7 +214,7 @@ export default hopeTheme({
 
     // These features are enabled for demo, only preserve features you need here
     mdEnhance: {
-     
+      footnote: true,
       align: true,
       attrs: true,
       codetabs: true,
